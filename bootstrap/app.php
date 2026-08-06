490980637465df9f7bff2e7a5ad8fb0d2ff6863d
slashcutz <slashcutz@users.noreply.github.com>
2026-08-06 20:03:43 +0530

    Fix asset URLs by configuring TrustProxies for Render

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->append(\App\Http\Middleware\SetDeviceCookie::class);
 
         $middleware->alias([
