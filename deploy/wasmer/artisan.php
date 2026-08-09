92260b65214a038bcd4b3e5f557018f383315f4d
slashcutz <slashcutz@users.noreply.github.com>
2026-08-09 23:03:15 +0530

    fix(wasmer): create Laravel storage framework dirs (sessions/cache/views/logs) during deploy jobs - volume mounts empty

--- deploy/wasmer/artisan.php
+++ deploy/wasmer/artisan.php
@@ -13,6 +13,17 @@ if (!file_exists($dbFile)) {
     touch($dbFile);
 }
 
+foreach ([
+    '/app/storage/framework/cache/data',
+    '/app/storage/framework/sessions',
+    '/app/storage/framework/views',
+    '/app/storage/logs',
+] as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0777, true);
+    }
+}
+
 $GLOBALS['argv'] = array_merge(['artisan'], array_slice($_SERVER['argv'] ?? $argv ?? [], 1));
 $_SERVER['argv'] = $GLOBALS['argv'];
 $_SERVER['argc'] = count($GLOBALS['argv']);
